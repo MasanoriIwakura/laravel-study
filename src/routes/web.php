0b5f6afd5be9b849cc071aec0885509261bcd12d
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-17 07:16:11 +0000

    Update routes
    ルーティングを試してみる

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function () {
+    return '<html><body><h1>Hello</h1></body></html>';
+});
+
+$html = <<<EOF
+<html>
+<body>
+    <h1>Hoge</h1>
+    <p>ヒアドキュメント</p>
+</body>
+</html>
+EOF;
+
+Route::get('hoge', function () use ($html) {
+    return $html;
+});
